11656376492a5eb4282fad23eceff5b7e4371527
ZiedHf <ziedhaffoudhi@gmail.com>
2017-10-23 15:48:14 +0100

    ZiedHf branch first commit

--- src/App.js
+++ src/App.js
@@ -2,9 +2,46 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 import {search} from './lib/SpotifyUtil'
 
-class App extends Component {
+class SearchList extends Component{
+  constructor(props){
+    super(props)
+    this.state = {
+      search_result: undefined
+    }
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <pre style={{textAlign: 'left'}}>{this.props.search_result}</pre>
+      </div>
+
+      // <div className="row">
+      //   <div className="col-4">
+      //     <div className="list-group" id="list-tab" role="tablist">
+      //       <a className="list-group-item list-group-item-action active" id="list-home-list" data-toggle="list" href="#list-home" role="tab" aria-controls="home">Home</a>
+      //       <a className="list-group-item list-group-item-action" id="list-profile-list" data-toggle="list" href="#list-profile" role="tab" aria-controls="profile">Profile</a>
+      //       <a className="list-group-item list-group-item-action" id="list-messages-list" data-toggle="list" href="#list-messages" role="tab" aria-controls="messages">Messages</a>
+      //       <a className="list-group-item list-group-item-action" id="list-settings-list" data-toggle="list" href="#list-settings" role="tab" aria-controls="settings">Settings</a>
+      //     </div>
+      //   </div>
+      //   <div className="col-8">
+      //     <div className="tab-content" id="nav-tabContent">
+      //       <div className="tab-pane fade show active" id="list-home" role="tabpanel" aria-labelledby="list-home-list">...</div>
+      //       <div className="tab-pane fade" id="list-profile" role="tabpanel" aria-labelledby="list-profile-list">...</div>
+      //       <div className="tab-pane fade" id="list-messages" role="tabpanel" aria-labelledby="list-messages-list">...</div>
+      //       <div className="tab-pane fade" id="list-settings" role="tabpanel" aria-labelledby="list-settings-list">...</div>
+      //     </div>
+      //   </div>
+      // </div>
+    );
+  }
+}
+
+class Form extends Component {
   constructor(props) {
     super(props)
     this.state = {
@@ -16,28 +53,28 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="card">
+        <h4 className="card-header">Welcome To Smartify Application</h4>
+        <div className="card-body">
+          <h4 className="card-title">What Are You Looking For ?</h4>
+          <p className="card-text">
+            <input type='text' onChange={ e => this.setState({search_text: e.target.value})} value={(this.state != null) ? this.state.search_text : ''} /><br/>
+  
+            <input type='radio' checked={(this.state != null) ? this.state.search_type === 'artist' : 0} onChange={e => this.setState({search_type: 'artist'})} /> Artist
+            <input type='radio' checked={(this.state != null) ? this.state.search_type === 'album' : 0} onChange={e => this.setState({search_type: 'album'})} /> Album
+            <input type='radio' checked={(this.state != null) ? this.state.search_type === 'track' : 0} onChange={e => this.setState({search_type: 'track'})} /> Track
+            <br/>
+            <button onClick={ e => this.doSearch()} className="btn btn-primary">Search</button>
+            {
+              (this.state != null) ? (
+                this.state.search_result ?
+                <SearchList search_result={JSON.stringify(this.state.search_result, false, 3)}/>
+                : undefined
+              )
+              : undefined
+            }
+          </p>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-        <input type='text' onChange={ e => this.setState({search_text: e.target.value})} value={this.state.search_text} /><br/>
-
-        <input type='radio' checked={this.state.search_type === 'artist'} onChange={e => this.setState({search_type: 'artist'})} /> Artist
-        <input type='radio' checked={this.state.search_type === 'album'} onChange={e => this.setState({search_type: 'album'})} /> Album
-        <input type='radio' checked={this.state.search_type === 'track'} onChange={e => this.setState({search_type: 'track'})} /> Track
-        <br/>
-        <button onClick={ e => this.doSearch()}>Search</button>
-        {
-          (
-            this.state.search_result ?
-              <pre style={{textAlign: 'left'}}>{JSON.stringify(this.state.search_result, false, 3)}</pre>
-            : undefined
-          )
-        }
       </div>
     );
   }
@@ -49,6 +86,16 @@ class App extends Component {
       json => {
         this.setState({search_result: json})
     })
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Form />
+      </div>
+    );
   }
 }
 
