75f648f15f4ef411cc3a6fd871fe25f067ba2e4d
Zied Hf <ziedhaffoudhi@gmail.com>
2017-11-20 10:07:40 +0100

    Add index.js to the components folder #14

---
+++ src/components/index.js
@@ -0,0 +1,17 @@
+import { AlbumComponent } from './AlbumComponent';
+import { ArtistComponent } from './ArtistComponent';
+import { TrackComponent } from './TrackComponent';
+import { SearchFormComponent } from './SearchFormComponent';
+import { SearchResultComponent } from './SearchResultComponent';
+import { ErrorComponent } from './ErrorComponent';
+import { FavoritesComponent } from './FavoritesComponent';
+
+export {
+  AlbumComponent,
+  ArtistComponent,
+  TrackComponent,
+  SearchFormComponent,
+  SearchResultComponent,
+  ErrorComponent,
+  FavoritesComponent
+};
